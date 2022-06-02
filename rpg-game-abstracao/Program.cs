01538cde8be288b0472f417caf3dcae56cb96c49
Gustavo Augusto F <91622682+FerrGusttavo@users.noreply.github.com>
2022-06-02 17:00:49 -0300

    adicionando projeto

---
+++ rpg-game-abstracao/Program.cs
@@ -0,0 +1,17 @@
+﻿using RPGame.src.Entities;
+
+namespace RPGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard jennica = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(jennica.Attack(1));
+            Console.WriteLine(jennica.Attack(7));
+        }
+    }
+}
